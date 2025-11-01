bbb4e35b1bddf21b19aa286eeea2c21662d43421
tk <tk@163.com>
2025-11-01 18:58:27 +0800

    Initial commit: 每日截图工具

---
+++ test/src/com/yyk/Main.java
@@ -0,0 +1,11 @@
+package com.yyk;
+
+/**
+ * @author tk
+ */
+public class Main {
+    public static void main(String[] args) {
+        // DailyScreenshotScheduler.startScreenshotSchedulerWithWatermark(true,13,14);
+        DailyScreenshotScheduler.startRandomScreenshotSchedulerWithWatermark(true);
+    }
+}
